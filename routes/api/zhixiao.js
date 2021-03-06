eb1cae0241936f8bd0911555de71719a2e10de02
Hjh8 <50100494+Hjh8@users.noreply.github.com>
2021-03-06 20:48:59 +0800

    完善代码

--- routes/api/zhixiao.js
+++ routes/api/zhixiao.js
@@ -6,7 +6,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '5642818',
+  password : 'home',
   database : 'zhixiao'
 })
 
